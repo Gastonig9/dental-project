9f50f73509f31b4248aebe8a5f702c763eac1e00
Paula <paulaoorjuela@gmail.com>
2024-06-19 18:12:21 -0500

    add sections (navbar and CallToAction)

--- dental-landing/src/Render/sections/Landing/CallToAction.tsx
+++ dental-landing/src/Render/sections/Landing/CallToAction.tsx
@@ -1,44 +1,32 @@
-import { Hero } from "../../components/Landing/CallToAction/Hero";
-import { Links } from "../../components/Landing/CallToAction/Links";
-import { Stats } from "../../components/Landing/CallToAction/Stats";
+// import { Hero } from "../../components/Landing/CallToAction/Hero";
+// import { Links } from "../../components/Landing/CallToAction/Links";
+// import { Stats } from "../../components/Landing/CallToAction/Stats";
 
 export const CalltoAction = () => {
   return (
-    <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-      {/* <img
-        src=""
-        alt="img"
-        className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
-      /> */}
-      <div
-        className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
-        aria-hidden="true"
-      >
-        <div
-          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        />
-      </div>
-      <div
-        className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
-        aria-hidden="true"
-      >
-        <div
-          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        />
-      </div>
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <Hero></Hero>
-        <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-          <Links></Links>
-          <Stats></Stats>
+    <div className="relative isolate overflow-hidden bg-background py-20">
+      <div className="flex flex-col sm:flex-row justify-around items-center mx-10 px-6 lg:px-8">
+        <div className="mx-auto lg:mx-0 w-6/12">
+          <h2 className="mb-10 tracking-tight text-3xl w-7/12 text-black poppins-bold">
+            Sonrisas Saludables, Vidas Felices
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-black poppins-regular text-justify">
+            En Somos Green Pol, brindamos cuidados odontológicos de excelencia,
+            desde chequeos rutinarios y cirugías hasta odontopediatría y
+            endodoncia. Creando un ambiente acogedor y seguro, hacemos que cada
+            visita sea una experiencia agradable. Ven y descubre cómo lograr una
+            sonrisa más saludable y radiante.
+          </p>
+          <div className="mt-40">
+            <a href="">
+              <button className="bg-acento p-3 rounded-lg poppins-semibold">
+                Agendar turno
+              </button>
+            </a>
+          </div>
+        </div>
+        <div className="flex justify-center items-center mx-auto w-6/12">
+          <div className="bg-darkgray w-[500px] h-[500px] rounded-3xl shadow-2xl"></div>
         </div>
       </div>
     </div>
