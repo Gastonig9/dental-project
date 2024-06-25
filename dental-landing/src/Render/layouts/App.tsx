c75d4de61a57af9e7023be205e853256540f7c57
Brian Cordoba <briancormin@gmail.com>
2024-06-25 16:19:32 -0500

    Added Navbar to platform/panel

--- dental-landing/src/Render/layouts/App.tsx
+++ dental-landing/src/Render/layouts/App.tsx
@@ -1,3 +1,6 @@
+import Example from "../pages/Example/Example";
+import Example2 from "../pages/Example2/Example2";
+import Example3 from "../pages/Example3/Example3";
 import { Landing } from "../pages/Landing/Landing";
 import Login from "../pages/Login/Login";
 import NotFound from "../pages/NotFound/NotFound";
@@ -9,6 +12,9 @@ function App() {
       <Route path="/" element={ <Landing /> } />
       <Route path="/login" element={ <Login /> } />
       <Route path="/*" element={ <NotFound /> } />
+      <Route path="/example" element={ <Example /> } />
+      <Route path="/example2" element={ <Example2 /> } />
+      <Route path="/example3" element={ <Example3 /> } />
     </Routes>
   );
 }
---
+++ dental-landing/src/Render/pages/Example2/Example2.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/Platform/Navbar'
+
+const Example2 = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Example2
