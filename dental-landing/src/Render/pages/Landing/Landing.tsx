a9088f9c7a16fd1682dc7981ef77ab790d3f1587
Paula <paulaoorjuela@gmail.com>
2024-06-25 09:21:24 -0500

    add responsive navbar, callToAction

--- dental-landing/src/Render/pages/Landing/Landing.tsx
+++ dental-landing/src/Render/pages/Landing/Landing.tsx
@@ -1,8 +1,21 @@
+import WhatsappBtn from "../../components/Landing/WhatsappBtn";
+import { Header } from "../../components/UI/Header/Header";
+import { AboutUs } from "../../sections/About-Us/AboutUs";
+import { CalltoAction } from "../../sections/Landing/CallToAction";
+import { FAQ } from "../../sections/Landing/FAQ";
+import { Footer } from "../../sections/Landing/Footer";
+import { Specialties } from "../../sections/Specialties/Specialties";
 
 export const Landing = () => {
   return (
     <>
-      
+    <Header></Header>
+    <CalltoAction></CalltoAction>
+    <Specialties></Specialties>
+    <AboutUs></AboutUs>
+    <FAQ></FAQ>
+    <Footer></Footer>
+    <WhatsappBtn></WhatsappBtn>
     </>
   );
 };
