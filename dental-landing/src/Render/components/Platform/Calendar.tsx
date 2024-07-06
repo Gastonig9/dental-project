d02aa53a0a454415e5da876192e4e21f5d93c6bc
Lucas Pirez <lucaspirez42@gmail.com>
2024-07-06 13:19:54 -0300

    fix types

--- dental-landing/src/Render/components/Platform/Calendar.tsx
+++ dental-landing/src/Render/components/Platform/Calendar.tsx
@@ -1,17 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect, useState } from "react";
-import FullCalendar from "@fullcalendar/react";
-import { EventContentArg } from "@fullcalendar/core/index.js";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction";
-import esLocale from "@fullcalendar/core/locales/es";
-import cancelAppointment from "../../../assets/img/calendar/Cancel.png";
-import viewAppointment from "../../../assets/img/calendar/Note.png";
-import { Link } from "react-router-dom";
-import Swal from "sweetalert2";
-import axios from "axios";
-import { Appointment } from "../../../types/dtos/appointment/appointment.type";
+import { useEffect, useState } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import { EventContentArg } from '@fullcalendar/core/index.js';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import esLocale from '@fullcalendar/core/locales/es';
+import cancelAppointment from '../../../assets/img/calendar/Cancel.png';
+import viewAppointment from '../../../assets/img/calendar/Note.png';
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import axios from 'axios';
+import { Appointment } from '../../../types/dtos/appointment/appointment.type';
 
 export const Calendar = () => {
   const [appointments, setAppointments] = useState<Appointment[]>([]);
@@ -19,7 +19,7 @@ export const Calendar = () => {
   useEffect(() => {
     const getAppointments = async () => {
       const responseAppointments = await fetch(
-        "http://localhost:3000/api/appointments/"
+        'http://localhost:3000/api/appointments/'
       );
       const dataAppointments = await responseAppointments.json();
       setAppointments(dataAppointments);
@@ -30,33 +30,39 @@ export const Calendar = () => {
 
   const handleDelete = async (eventInfo: EventContentArg) => {
     const result = await Swal.fire({
-      title: "¿Estás seguro que deseas cancelar el turno?",
-      text: "Esta accion es irreversible",
-      icon: "warning",
+      title: '¿Estás seguro que deseas cancelar el turno?',
+      text: 'Esta accion es irreversible',
+      icon: 'warning',
       showCancelButton: true,
-      confirmButtonColor: "#d33",
-      cancelButtonColor: "#3085d6",
-      confirmButtonText: "Cancelar",
-      cancelButtonText: "Volver",
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
+      confirmButtonText: 'Cancelar',
+      cancelButtonText: 'Volver',
     });
 
     if (result.isConfirmed) {
       const id = parseInt(eventInfo.event.id);
 
       try {
-        const response = await axios.delete(`http://localhost:3000/api/appointments/${id}`);
+        const response = await axios.delete(
+          `http://localhost:3000/api/appointments/${id}`
+        );
 
         if (response.status === 200) {
-          Swal.fire("Turno cancelado", "El turno ha sido cancelado con exito.", "success");
+          Swal.fire(
+            'Turno cancelado',
+            'El turno ha sido cancelado con exito.',
+            'success'
+          );
 
           setAppointments((prevAppointments) =>
             prevAppointments.filter((appointment) => appointment.id !== id)
           );
         } else {
-          Swal.fire("Error", "No se pudo eliminar el turno.", "error");
+          Swal.fire('Error', 'No se pudo eliminar el turno.', 'error');
         }
       } catch (error) {
-        Swal.fire("Error", "Ocurrió un error al eliminar el turno.", "error");
+        Swal.fire('Error', 'Ocurrió un error al eliminar el turno.', 'error');
       }
     }
   };
@@ -66,18 +72,19 @@ export const Calendar = () => {
       <div className="custom-event">
         <span>{eventInfo.timeText}</span>
         <span>{eventInfo.event.title}</span>
-        {eventInfo.view.type === "timeGridDay" && (
+        {eventInfo.view.type === 'timeGridDay' && (
           <>
             <span className="paciente">
-              Paciente:{" "}
+              Paciente:{' '}
               <b>
-                {eventInfo.event.extendedProps.patient.name}{" "}
+                {eventInfo.event.extendedProps.patient.name}{' '}
                 {eventInfo.event.extendedProps.patient.surname}
               </b>
             </span>
             <div className="event-buttons">
               <div>
-                <Link to={`/patient-management/seeEditPatient/${eventInfo.event.extendedProps.patient.id}`}>
+                <Link
+                  to={`/patient-management/seeEditPatient/${eventInfo.event.extendedProps.patient.id}`}>
                   <img src={viewAppointment} alt="Ver" />
                 </Link>
               </div>
@@ -93,26 +100,31 @@ export const Calendar = () => {
 
   const handleDateClick = (arg: any) => {
     const calendarApi = arg.view.calendar;
-    calendarApi.changeView("timeGridDay", arg.dateStr);
+    calendarApi.changeView('timeGridDay', arg.dateStr);
   };
+
+  const formatAppointments = appointments.map((appointment) => ({
+    ...appointment,
+    id: appointment?.id?.toString(),
+  }));
 
   return (
     <div className="calendar-contain">
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
-        events={appointments}
+        events={formatAppointments}
         eventContent={renderEventContent}
         headerToolbar={{
-          left: "prev,next today",
-          center: "title",
-          right: "dayGridMonth,timeGridWeek,timeGridDay",
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay',
         }}
         locale={esLocale}
         dateClick={handleDateClick}
         eventTimeFormat={{
-          hour: "2-digit",
-          minute: "2-digit",
+          hour: '2-digit',
+          minute: '2-digit',
           meridiem: false,
         }}
       />
