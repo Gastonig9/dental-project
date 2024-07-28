b451ec1d9bd04e96bb9ac84de8c12085de529051
Paula <paulaoorjuela@gmail.com>
2024-07-28 18:14:39 -0500

    fix route to patientContext in PersonalInfo

--- dental-landing/src/Render/sections/PatientManagement/PersonalInfo.tsx
+++ dental-landing/src/Render/sections/PatientManagement/PersonalInfo.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import axios, { AxiosError } from "axios";
 import Swal from "sweetalert2";
-import { usePatientContext } from "../../pages/contexts/patientContext";
+import { usePatientContext } from "../../../Features/contexts/patientContext";
 import { Patient } from "../../../types/dtos/Patient/NewPatient.type";
 import { token } from "../../../localStorage/token";
 
